640b254232cdefb807444fb889a54b3405d9db8a
NoahCounsell <noahwcounsell@icloud.com>
2025-02-14 14:32:19 +0000

    redo all of it

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-
 const tailwindScript = document.createElement('script');
 tailwindScript.src = "https://cdn.tailwindcss.com";
 document.head.appendChild(tailwindScript);
 
+const element = document.querySelector('.c7e7df4d');
 
-var buttonContainers = document.getElementsByClassName('flex gap-x-1.5 text-token-text-primary');
-
-
-if (buttonContainers.length > 0) {
-  var buttonContainer = buttonContainers[0];
-  
-
-  var highlightButton = document.createElement('div');
-  highlightButton.textContent = 'Highlight';
-  highlightButton.classList.add('border bg-gray-100 rounded-full');
-  
-
-  buttonContainer.appendChild(highlightButton);
-
+if (element) {
+    element.style.color = 'blue';
 }
